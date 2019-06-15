9164dae1aba0e810d9cbeaf0895e3924e9f185c7
danielwaiforssell <daniel.waiforssell@mail.utoronto.ca>
2019-06-15 09:43:25 -0400

    Game has been completed

--- app/src/main/java/com/example/escalatorclimb/MainActivity.java
+++ app/src/main/java/com/example/escalatorclimb/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.escalatorclimb;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 //activity for the title screen
 public class MainActivity extends AppCompatActivity {
-
+    //global media player used for title screen music
+    MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +23,12 @@ public class MainActivity extends AppCompatActivity {
         Button startBtn = (Button) findViewById(R.id.startBtn);
         //create button for viewing the instructions
         Button instructionsBtn = (Button) findViewById(R.id.instructionsBtn);
-
+        //create button for exiting the game
+        Button exitBtn = (Button) findViewById(R.id.exitBtn);
+        //initialize the title screen music
+        player = MediaPlayer.create(getApplicationContext(),R.raw.characterselect);
+        //start title screen music
+        player.start();
         instructionsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,10 +40,20 @@ public class MainActivity extends AppCompatActivity {
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //stop the media player when the game starts
+                player.stop();
                 //create a new intent and link it to the second activity. provide link from main activity to second one
                 Intent startIntent = new Intent(getApplicationContext(),gameOver.class);
-                startActivity(startIntent);
+                startActivityForResult(startIntent,2);
                 //all happens within a button press
+            }
+        });
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //close when exit button pressed
+                finish();
+                System.exit(0);
             }
         });
     }
@@ -42,5 +61,13 @@ public class MainActivity extends AppCompatActivity {
     //disable the back button on the start menu
     public void onBackPressed(){
 
+    }
+
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        //code restarts music upon returning to this class
+        if((requestCode == 2)&& (resultCode == RESULT_OK)){
+            player = MediaPlayer.create(getApplicationContext(),R.raw.characterselect);
+            player.start();
+        }
     }
 }
